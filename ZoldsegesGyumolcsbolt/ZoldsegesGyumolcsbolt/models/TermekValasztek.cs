3a7bbaa8b23a44c32ad86e81349543e46ffc7565
b0szazal <zalan.szasz06@gmail.com>
2023-12-01 14:16:25 +0100

    live share test

---
+++ ZoldsegesGyumolcsbolt/ZoldsegesGyumolcsbolt/models/TermekValasztek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoldsegesGyumolcsbolt.models
+{
+    public class TermekValasztek
+    {
+        public List<string> termekek = new List<string>() {"Alma","Körte","Paprika","Paradicsom","Banán","Szőlő","Ananász","Barack","Szilva","Burgonya" };
+
+        public TermekValasztek() { }
+    }
+}
